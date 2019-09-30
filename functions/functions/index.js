7d27052bd348e98925beaa6e2d25268fe77c114f
salil499 <55999182+salil499@users.noreply.github.com>
2019-10-01 00:13:02 +0530

    Update index.js

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,17 +1,5 @@
 /**
- * Copyright 2018 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+
  */
 'use strict';
 
@@ -104,7 +92,5 @@ exports.addMessage = functions.https.onCall((data, context) => {
     // Re-throwing the error as an HttpsError so that the client gets the error details.
     throw new functions.https.HttpsError('unknown', error.message, error);
   });
-  // [END returnMessageAsync]
-  // [END_EXCLUDE]
 });
 // [END messageFunctionTrigger]
